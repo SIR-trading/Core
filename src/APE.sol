a87846a81313a75fc78cee803950eab42b79f9f5
Codonyat <nil.garcia@gmail.com>
2023-07-26 22:17:44 +0800

    Got rid off resettable balances in the contracts

--- src/APE.sol
+++ src/APE.sol
@@ -5,39 +5,24 @@ pragma solidity >=0.8.0;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {IVault} from "./interfaces/IVault.sol";
 
-// Libraries
-import {ResettableBalancesUInt216} from "./libraries/ResettableBalancesUInt216.sol";
-
 // Contracts
 import {Owned} from "./Owned.sol";
 
+/**
+ * @dev Modified from Solmate's ERC20.sol
+ */
 abstract contract APE is Owned, ERC20 {
     address public immutable debtToken;
     address public immutable collateralToken;
     int8 public immutable leverageTier;
 
-    /*///////////////////////////////////////////////////////////////
-                              IERC20 STORAGE
-    //////////////////////////////////////////////////////////////*/
-
     uint256 public totalSupply;
-
-    /**
-     * @notice APE is the base contract of all TEA & APE tokens because logic-wise TEA and APE are identical.
-     */
-    ResettableBalancesUInt216.ResettableBalances private _balances;
-
+    mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
-
-    /*///////////////////////////////////////////////////////////////
-                           EIP-2612 STORAGE
-    //////////////////////////////////////////////////////////////*/
 
     bytes32 public constant PERMIT_TYPEHASH =
         keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-
     uint256 private immutable INITIAL_CHAIN_ID;
-
     bytes32 private immutable INITIAL_DOMAIN_SEPARATOR;
 
     mapping(address => uint256) public nonces;
@@ -69,26 +54,32 @@ abstract contract APE is Owned, ERC20 {
         return true;
     }
 
-    function balanceOf(address account) external view returns (uint256) {
-        return _balances.get(account);
-    }
+    function transfer(address to, uint256 amount) public virtual returns (bool) {
+        balanceOf[msg.sender] -= amount;
 
-    function transfer(address to, uint256 amount) external returns (bool) {
-        _balances.decrease(msg.sender, amount);
-        _balances.increase(to, amount);
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
 
         emit Transfer(msg.sender, to, amount);
 
         return true;
     }
 
-    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
-        if (allowance[from][msg.sender] != type(uint256).max) {
-            allowance[from][msg.sender] -= amount;
-        }
+    function transferFrom(address from, address to, uint256 amount) public virtual returns (bool) {
+        uint256 allowed = allowance[from][msg.sender]; // Saves gas for limited approvals.
 
-        _balances.decrease(from, amount);
-        _balances.increase(to, amount);
+        if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - amount;
+
+        balanceOf[from] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
 
         emit Transfer(from, to, amount);
 
@@ -107,22 +98,37 @@ abstract contract APE is Owned, ERC20 {
         uint8 v,
         bytes32 r,
         bytes32 s
-    ) external {
+    ) public virtual {
         require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
 
         // Unchecked because the only math done is incrementing
         // the owner's nonce which cannot realistically overflow.
         unchecked {
-            bytes32 digest = keccak256(
-                abi.encodePacked(
-                    "\x19\x01",
-                    DOMAIN_SEPARATOR(),
-                    keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
-                )
+            address recoveredAddress = ecrecover(
+                keccak256(
+                    abi.encodePacked(
+                        "\x19\x01",
+                        DOMAIN_SEPARATOR(),
+                        keccak256(
+                            abi.encode(
+                                keccak256(
+                                    "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+                                ),
+                                owner,
+                                spender,
+                                value,
+                                nonces[owner]++,
+                                deadline
+                            )
+                        )
+                    )
+                ),
+                v,
+                r,
+                s
             );
 
-            address recoveredAddress = ecrecover(digest, v, r, s);
-            require(recoveredAddress != address(0) && recoveredAddress == owner, "INVALID_PERMIT_SIGNATURE");
+            require(recoveredAddress != address(0) && recoveredAddress == owner, "INVALID_SIGNER");
 
             allowance[recoveredAddress][spender] = value;
         }
@@ -130,17 +136,17 @@ abstract contract APE is Owned, ERC20 {
         emit Approval(owner, spender, value);
     }
 
-    function DOMAIN_SEPARATOR() public view returns (bytes32) {
-        return block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : _computeDomainSeparator();
+    function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
+        return block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
     }
 
-    function _computeDomainSeparator() private view returns (bytes32) {
+    function computeDomainSeparator() internal view virtual returns (bytes32) {
         return
             keccak256(
                 abi.encode(
                     keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
                     keccak256(bytes(name)),
-                    keccak256(bytes("1")),
+                    keccak256("1"),
                     block.chainid,
                     address(this)
                 )
@@ -154,14 +160,17 @@ abstract contract APE is Owned, ERC20 {
     function mint(address to, uint256 amount) external onlyOwner {
         totalSupply += amount;
 
-        // Cannot overflow because the sum of all user balances can't exceed the max uint256 value.
-        _balances.increase(to, amount);
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
 
         emit Transfer(address(0), to, amount);
     }
 
     function burn(address from, uint256 amount) external onlyOwner {
-        _balances.decrease(from, amount);
+        balanceOf[from] -= amount;
 
         // Cannot underflow because a user's balance
         // will never be larger than the total supply.
@@ -170,32 +179,5 @@ abstract contract APE is Owned, ERC20 {
         }
 
         emit Transfer(from, address(0), amount);
-    }
-
-    /**
-     * @notice It resets all balances to 0
-     */
-    function liquidate() external onlyOwner {
-        if (totalSupply == 0) return;
-
-        _balances.reset();
-
-        emit Liquidation(totalSupply);
-        totalSupply = 0;
-    }
-
-    /**
-     *  @notice All holders could potentially be liquidated.
-     *  @return true if all balances and the token supply are 0
-     */
-    function isLiquidated() external view returns (bool) {
-        return totalSupply == 0;
-    }
-
-    /**
-     * @return number of times MAAM has been liquidated
-     */
-    function numberOfLiquidations() public view returns (uint216) {
-        return _balances.numLiquidations;
     }
 }
