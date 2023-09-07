494b5733847093345c0ab8c048c8f05840f6b04d
Codonyat <nil.garcia@gmail.com>
2023-09-07 11:30:30 +0200

    Forget about APE tokena addresses starting with a9e. It adds unncesseray costs in gas and bytecode.

--- src/libraries/SaltedAddress.sol
+++ src/libraries/SaltedAddress.sol
@@ -5,20 +5,16 @@ library SaltedAddress {
     bytes32 private constant _HASH_CREATION_CODE_APE =
         0x36af10401bb78153f4e90dbeb1fad843a0704d3884a00ab6d41ebca444f24edd; // keccak256(abi.encodePacked(vm.getCode("APE.sol:APE")))
 
-    function getSalt(uint256 vaultId) internal pure returns (bytes32 saltAPE) {
-        /**
-            DUMMY IMPLEMENTATION
-            I WANT TO GET MINED SALTS THAT RETURN ADDRESSES WITH PREFIXES a9e 
-         */
-        saltAPE = bytes32(vaultId);
-    }
-
     function getAddress(uint256 vaultId) internal view returns (address) {
-        bytes32 salt = getSalt(vaultId);
-
         return
             address(
-                uint160(uint(keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, _HASH_CREATION_CODE_APE))))
+                uint160(
+                    uint(
+                        keccak256(
+                            abi.encodePacked(bytes1(0xff), address(this), bytes32(vaultId), _HASH_CREATION_CODE_APE)
+                        )
+                    )
+                )
             );
     }
 }
