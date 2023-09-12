127413e23b546156cc75fe55f97553b9e4866701
Codonyat <nil.garcia@gmail.com>
2023-09-12 15:06:21 +0200

    Reinstalling Uniswap solc 8 required version of openzeppelin

--- test/Oracle.t.sol
+++ test/Oracle.t.sol
@@ -2,11 +2,10 @@
 pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
-import {UniswapV3Factory} from "uniswap-v3-core/UniswapV3Factory.sol";
+import {UniswapV3Factory} from "v3-core/UniswapV3Factory.sol";
 import {NonfungiblePositionManager} from "v3-periphery/NonfungiblePositionManager.sol";
 import {NonfungibleTokenPositionDescriptor} from "v3-periphery/NonfungibleTokenPositionDescriptor.sol";
 import {SwapRouter} from "v3-periphery/SwapRouter.sol";
-import {WETH9} from "lib/canonical-weth/contracts/WETH9.sol";
 import {Oracle} from "src/Oracle.sol";
 import {MockERC20} from "src/test/MockERC20.sol";
 
@@ -14,7 +13,6 @@ import {MockERC20} from "src/test/MockERC20.sol";
 
 contract OracleNotInitializedTest is Test {
     UniswapV3Factory private _uV3factory;
-    WETH9 private _WETH9;
     NonfungiblePositionManager private _uV3positionManager;
     Oracle private _oracle;
     MockERC20 private _tokenA;
@@ -28,16 +26,14 @@ contract OracleNotInitializedTest is Test {
         _uV3factory = new UniswapV3Factory();
         _uV3factory.enableFeeAmount(100, 1); // Add 1 bp fee tier
 
-        _WETH9 = new WETH9();
-
         NonfungibleTokenPositionDescriptor uV3tokenDescriptor = new NonfungibleTokenPositionDescriptor(
-            address(_WETH9),
+            address(0), // We do not use WETH9 anyway
             "ETH"
         );
 
         _uV3positionManager = new NonfungiblePositionManager(
             address(_uV3factory),
-            address(_WETH9),
+            address(0),
             address(uV3tokenDescriptor)
         ); // It has createAndInitializePoolIfNecessary
 
