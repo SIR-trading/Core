0278d00e59014d539d5c1e440f632606b078d110
Codonyat <nil.garcia@gmail.com>
2024-02-08 16:56:05 +0100

    Reversed the role of ETH and WETH in the Staker contract

--- src/ERC20Staker.sol
+++ src/ERC20Staker.sol
@@ -2,7 +2,9 @@
 pragma solidity >=0.8.0;
 
 import {SystemConstants} from "./libraries/SystemConstants.sol";
+import {Addresses} from "./libraries/Addresses.sol";
 import {Vault} from "./Vault.sol";
+import {WETH} from "solmate/tokens/WETH.sol";
 
 /** @notice Solmate mod
     @dev SIR balance is designed to fit in a 80-bit unsigned integer.
@@ -13,12 +15,16 @@ contract ERC20Staker {
     error NewAuctionCannotStartYet();
     error TokensAlreadyClaimed();
     error AuctionIsNotOver();
+    error AuctionIsOver();
+    error BidTooLow();
+    error NoDividends();
 
     event Transfer(address indexed from, address indexed to, uint256 amount);
     event Approval(address indexed owner, address indexed spender, uint256 amount);
     event Staked(address indexed staker, uint256 amount);
     event Unstaked(address indexed staker, uint256 amount);
 
+    WETH private immutable _WETH = WETH(Addresses.ADDR_WETH);
     Vault internal immutable VAULT;
 
     string public name = "Sustainable Investing Returns";
@@ -32,17 +38,19 @@ contract ERC20Staker {
 
     struct Balance {
         uint80 balanceOfSIR; // Amount of transferable SIR
-        uint96 unclaimedWETH; // Amount of WETH owed to the staker(s)
+        uint96 unclaimedETH; // Amount of WETH owed to the staker(s)
     }
 
     struct Auction {
         address bidder; // Address of the bidder
+        uint96 bid; // Amount of the bid
         uint40 startTime; // Auction start time
         bool winnerPaid; // Whether the winner has been paid
     }
 
     StakingParams internal stakingParams; // Total staked SIR and cumulative ETH per SIR
     Balance internal supply; // Total unstaked SIR and WETH owed to the stakers
+    uint96 internal totalBids; // Total amount of WETH in the contract
 
     mapping(address token => Auction) public auctions;
     // mapping(address token => WinningBidder) public winningBidders;
@@ -214,6 +222,30 @@ contract ERC20Staker {
                             STAKING FUNCTIONS 
     ////////////////////////////////////////////////////////////////*/
 
+    function stake(uint80 amount) external {
+        Balance memory balance = balances[msg.sender];
+        StakingParams memory stakingParams_ = stakingParams;
+        StakingParams memory stakerParams = stakersParams[msg.sender];
+
+        uint80 newBalanceOfSIR = balance.balanceOfSIR - amount;
+
+        unchecked {
+            // Update balance
+            balances[msg.sender] = Balance(newBalanceOfSIR, _dividends(balance, stakingParams_, stakerParams));
+
+            // Update staker info
+            stakersParams[msg.sender] = StakingParams(stakerParams.stake + amount, stakingParams_.cumETHPerSIRx80);
+
+            // Update supply
+            supply.balanceOfSIR -= amount;
+
+            // Update total stake
+            stakingParams.stake = stakingParams_.stake + amount;
+
+            emit Staked(msg.sender, amount);
+        }
+    }
+
     function unstake(uint80 amount) external {
         Balance memory balance = balances[msg.sender];
         StakingParams memory stakingParams_ = stakingParams;
@@ -241,46 +273,25 @@ contract ERC20Staker {
         }
     }
 
-    function stake(uint80 amount) external {
-        Balance memory balance = balances[msg.sender];
-        StakingParams memory stakingParams_ = stakingParams;
-        StakingParams memory stakerParams = stakersParams[msg.sender];
-
-        uint80 newBalanceOfSIR = balance.balanceOfSIR - amount;
-
-        unchecked {
-            // Update balance
-            balances[msg.sender] = Balance(newBalanceOfSIR, _dividends(balance, stakingParams_, stakerParams));
-
-            // Update staker info
-            stakersParams[msg.sender] = StakingParams(stakerParams.stake + amount, stakingParams_.cumETHPerSIRx80);
-
-            // Update supply
-            supply.balanceOfSIR -= amount;
-
-            // Update total stake
-            stakingParams.stake = stakingParams_.stake + amount;
-
-            emit Staked(msg.sender, amount);
-        }
-    }
-
     function claim() external {
         unchecked {
             StakingParams memory stakingParams_ = stakingParams;
             uint96 dividends_ = _dividends(balances[msg.sender], stakingParams_, stakersParams[msg.sender]);
 
             // Null the unclaimed dividends
-            balances[msg.sender].unclaimedWETH = 0;
+            balances[msg.sender].unclaimedETH = 0;
 
             // Update staker info
             stakersParams[msg.sender].cumETHPerSIRx80 = stakingParams_.cumETHPerSIRx80;
 
             // Update ETH supply in the contract
-            supply.unclaimedWETH -= dividends_;
+            supply.unclaimedETH -= dividends_;
 
-            // WETH is immutable so we can use the low-level call
-            Addresses.ADDR_WETH.call(abi.encodeWithSignature("transfer(address,uint256)", msg.sender, dividends_));
+            // Transfer dividends
+            payable(msg.sender).transfer(dividends_);
+
+            // // WETH is immutable and we know it won't revert
+            // Addresses.ADDR_WETH.call(abi.encodeWithSignature("transfer(address,uint256)", msg.sender, dividends_));
         }
     }
 
@@ -295,8 +306,8 @@ contract ERC20Staker {
     ) private pure returns (uint96) {
         unchecked {
             return
-                balance.unclaimedWETH +
-                uint96( // Safe to cast to uint96 because supply.unclaimedWETH is uint96
+                balance.unclaimedETH +
+                uint96( // Safe to cast to uint96 because supply.unclaimedETH is uint96
                     (uint256(stakingParams_.cumETHPerSIRx80 - stakerParams.cumETHPerSIRx80) * stakerParams.stake) >> 80
                 );
         }
@@ -309,27 +320,36 @@ contract ERC20Staker {
     // WHAT IF STAKE IS 0??????????????
     // WHAT IF TRANSFER REVERTS?????
 
+    // TAKE INTO CONSIDERATION THERE ARE MANY AUCTIONS BIDDING!
+    // I NEED TO TRACK TOTAL OF ALL BIDS
     function bid(address token) external payable {
         Auction memory auction = auctions[token];
 
         if (block.timestamp >= auction.startTime + SystemConstants.AUCTION_DURATION) revert AuctionIsOver();
 
-        // If the bidder is the current winner, we just top up the bid
-        if (msg.sender == auction.bidder) return;
+        // Get the current bid
+        uint96 totalBids_ = totalBids;
+        uint96 newBid = uint96(_WETH.balanceOf(address(this)) - totalBids_);
+
+        // If the bidder is the current winner, we just increase the bid
+        if (msg.sender == auction.bidder) {
+            auctions[token].bid += newBid;
+            totalBids = totalBids_ + newBid - auction.bid;
+            return;
+        }
 
         // If the bidder is not the current winner, we check if the bid is higher
-        uint256 prevBid = address(this).balance - msg.value;
-        if (msg.value <= prevBid) revert BidTooLow();
+        if (newBid <= auction.bid) revert BidTooLow();
 
-        // Update bidder
-        auctions[token].bidder = msg.sender;
+        // Update bidder & bid
+        auctions[token] = Auction({bidder: msg.sender, bid: newBid, startTime: auction.startTime, winnerPaid: false});
 
-        // Transfer the previous bid to the contract
-        auction.bidder.transfer(prevBid);
+        // Return the previous bid
+        _WETH.transfer(auction.bidder, auction.bid);
     }
 
     function collectFeesAndStartAuction(address token) external {
-        // WETH is the dividend paying token, so we do not start an auction for it.
+        // (W)ETH is the dividend paying token, so we do not start an auction for it.
         if (token != Addresses.ADDR_WETH) {
             Auction memory auction = auctions[token];
 
@@ -338,10 +358,14 @@ contract ERC20Staker {
 
             // Start a new auction
             auctions[token] = Auction({
-                addr: address(0),
+                bidder: address(0),
+                bid: 0,
                 startTime: uint40(block.timestamp), // This automatically aborts any reentrancy attack
                 winnerPaid: false
             });
+
+            // Update totalBids
+            totalBids -= auction.bid;
 
             // We pay the previous winner if it has not been paid yet
             _payAuctionWinner(token, auction);
@@ -350,7 +374,7 @@ contract ERC20Staker {
         // Retrieve fees from the vault. Reverts if no fees are available.
         VAULT.withdrawFees(token);
 
-        // Distribute dividends even if paying the previous winner fails
+        // Distribute dividends from the previous auction even if paying the previous winner fails
         _distributeDividends();
     }
 
@@ -370,23 +394,28 @@ contract ERC20Staker {
 
     function _distributeDividends() private {
         unchecked {
-            // Get the total amount of WETH in the contract
-            uint256 newUnclaimedWETH = Addresses.ADDR_WETH.call(
-                abi.encodeWithSignature("balanceOf(address)", address(this))
-            );
+            // Any excess WETH in the contract will also be distributed.
+            uint256 excessWETH = _WETH.balanceOf(address(this)) - totalBids;
 
-            // Any excess WETH in the contracdt will also be distributed.
-            uint256 dividends = newUnclaimedWETH - supply.unclaimedWETH;
-            require(dividends > 0);
+            // Any excess ETH dropped by mistake in the contract
+            uint96 unclaimedETH = supply.unclaimedETH;
+            uint256 excessETH = address(this).balance - unclaimedETH;
+
+            // Compute dividends
+            uint256 dividends_ = excessWETH + excessETH;
+            if (dividends_ == 0) revert NoDividends();
+
+            // Unwrap WETH dividends to ETH
+            _WETH.withdraw(excessWETH);
 
             // Update cumETHPerSIRx80
             StakingParams memory stakingParams_ = stakingParams;
             stakingParams.cumETHPerSIRx80 =
                 stakingParams_.cumETHPerSIRx80 +
-                uint176((dividends << 80) / stakingParams_.stake);
+                uint176((dividends_ << 80) / stakingParams_.stake);
 
             // Update supply
-            supply.unclaimedWETH = uint96(newUnclaimedWETH);
+            supply.unclaimedETH = unclaimedETH + uint96(dividends_);
         }
     }
 
@@ -396,28 +425,25 @@ contract ERC20Staker {
         if (auction.winnerPaid) return false;
 
         // Only pay if there is a non-0 bid.
-        if (address(this).balance == 0) return false;
-
-        // Wrap ETH (bid) to WETH (dividends)
-        Addresses.ADDR_WETH.call{value: address(this).balance}(abi.encodeWithSignature("deposit()"));
+        if (auction.bid == 0) return false;
 
         /** Obtain reward amount
             Low-level call to avoid revert if the ERC20 token has some problems.
          */
         bytes memory data;
-        (success, data) = tokens[i].call(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
+        (success, data) = token.call(abi.encodeWithSignature("balanceOf(address)", address(this)));
 
         // balanceOf failed and we cannot continue
         if (!success || data.length != 32) return false;
 
         // Prize is 0, so the claim is successful but without a transfer
-        uint256 prize = abi.decode(data, (uint256));
-        if (prize == 0) return true;
+        uint256 tokenAmount = abi.decode(data, (uint256));
+        if (tokenAmount == 0) return true;
 
-        /** Pay the winner if prize > 0
+        /** Pay the winner if tokenAmount > 0
             Low-level call to avoid revert if the ERC20 token has some problems.
          */
-        (success, data) = token.call(abi.encodeWithSelector(ERC20.transfer.selector, auction.addr, prize));
+        (success, data) = token.call(abi.encodeWithSignature("transfer(address,uint256)", auction.bidder, tokenAmount));
 
         /** By the ERC20 standard, the transfer may go through without reverting (success == true),
             but if it returns a boolean that is false, the transfer actually failed.
