0a60118d08395b5aa4db4736961c0ab44d85dc66
Codonyat <nil.garcia@gmail.com>
2024-05-13 17:20:56 +0200

    Initial tests for Staker.t.sol

---
+++ test/Staker.t.sol
@@ -0,0 +1,195 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {SystemConstants} from "src/libraries/SystemConstants.sol";
+import {Vault} from "src/Vault.sol";
+import {Staker} from "src/Staker.sol";
+
+contract StakerTest is Test {
+    uint256 constant SLOT_SUPPLY = 4;
+    uint256 constant SLOT_BALANCES = 7;
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    Staker staker;
+    address alice;
+    address bob;
+    address charlie;
+
+    /// @dev Auxiliary function for minting APE tokens
+    function _mint(address account, uint80 amount) private {
+        // Increase total supply
+        uint256 slot = uint256(vm.load(address(staker), bytes32(uint256(SLOT_SUPPLY))));
+        uint80 balanceOfSIR = uint80(slot) + amount;
+        slot >>= 80;
+        uint96 unclaimedETH = uint96(slot);
+        vm.store(
+            address(staker),
+            bytes32(uint256(SLOT_SUPPLY)),
+            bytes32(abi.encodePacked(uint80(0), unclaimedETH, balanceOfSIR))
+        );
+        assertEq(staker.totalSupply(), balanceOfSIR, "Wrong slot used by vm.store");
+
+        // Increase balance
+        slot = uint256(vm.load(address(staker), keccak256(abi.encode(account, bytes32(uint256(SLOT_BALANCES))))));
+        balanceOfSIR = uint80(slot) + amount;
+        slot >>= 80;
+        unclaimedETH = uint96(slot);
+        vm.store(
+            address(staker),
+            keccak256(abi.encode(account, bytes32(uint256(SLOT_BALANCES)))),
+            bytes32(abi.encodePacked(uint80(0), unclaimedETH, balanceOfSIR))
+        );
+        assertEq(staker.balanceOf(account), balanceOfSIR, "Wrong slot used by vm.store");
+    }
+
+    function setUp() public {
+        Vault vault = new Vault(vm.addr(10), vm.addr(11), vm.addr(12));
+        staker = new Staker(vm.addr(13)); // Fake WETH address
+        staker.initialize(address(vault)); // Fake Vault address
+
+        alice = vm.addr(1);
+        bob = vm.addr(2);
+        charlie = vm.addr(3);
+    }
+
+    function _idToAddress(uint256 id) private pure returns (address) {
+        id = _bound(id, 1, 3);
+        return vm.addr(id);
+    }
+
+    function test_initialConditions() public {
+        assertEq(staker.supply(), 0);
+        assertEq(staker.totalSupply(), 0);
+        assertEq(staker.maxTotalSupply(), 0);
+
+        assertEq(staker.balanceOf(alice), 0);
+        assertEq(staker.totalBalanceOf(alice), 0);
+        assertEq(staker.balanceOf(bob), 0);
+        assertEq(staker.totalBalanceOf(bob), 0);
+
+        assertEq(staker.name(), "Safer Investment Rehypothecation");
+        assertEq(staker.symbol(), "SIR");
+        assertEq(staker.decimals(), SystemConstants.SIR_DECIMALS);
+    }
+
+    function testFuzz_approve(uint256 amount) public {
+        vm.prank(alice);
+        assertTrue(staker.approve(bob, amount));
+        assertEq(staker.allowance(alice, bob), amount);
+    }
+
+    function testFuzz_transfer(uint256 fromId, uint256 toId, uint80 transferAmount, uint80 mintAmount) public {
+        address from = _idToAddress(fromId);
+        address to = _idToAddress(toId);
+
+        transferAmount = uint80(_bound(transferAmount, 1, type(uint80).max));
+        mintAmount = uint80(_bound(mintAmount, transferAmount, type(uint80).max));
+
+        console.log("here");
+        _mint(from, mintAmount);
+        console.log("there");
+
+        vm.expectEmit();
+        emit Transfer(from, to, transferAmount);
+
+        vm.prank(from);
+        assertTrue(staker.transfer(to, transferAmount));
+
+        assertEq(staker.balanceOf(from), from == to ? mintAmount : mintAmount - transferAmount);
+        assertEq(staker.balanceOf(to), to == from ? mintAmount : transferAmount);
+    }
+
+    function testFuzz_transferMoreThanBalance(
+        uint256 fromId,
+        uint256 toId,
+        uint80 transferAmount,
+        uint80 mintAmount
+    ) public {
+        address from = _idToAddress(fromId);
+        address to = _idToAddress(toId);
+
+        transferAmount = uint80(_bound(transferAmount, 1, type(uint80).max));
+        mintAmount = uint80(_bound(mintAmount, 0, transferAmount - 1));
+
+        _mint(from, mintAmount);
+
+        vm.expectRevert();
+        staker.transfer(to, transferAmount);
+    }
+
+    function testFuzz_transferFrom(
+        uint256 fromId,
+        uint256 operatorId,
+        uint256 toId,
+        uint80 transferAmount,
+        uint80 mintAmount
+    ) public {
+        address operator = _idToAddress(operatorId);
+        address from = _idToAddress(fromId);
+        address to = _idToAddress(toId);
+
+        transferAmount = uint80(_bound(transferAmount, 1, type(uint80).max));
+        mintAmount = uint80(_bound(mintAmount, transferAmount, type(uint80).max));
+
+        _mint(from, mintAmount);
+
+        vm.prank(from);
+        assertTrue(staker.approve(operator, mintAmount));
+        assertEq(staker.allowance(from, operator), mintAmount);
+
+        vm.expectEmit();
+        emit Transfer(from, to, transferAmount);
+
+        vm.prank(operator);
+        assertTrue(staker.transferFrom(from, to, transferAmount));
+
+        assertEq(staker.allowance(from, operator), mintAmount - transferAmount);
+        if (operator != from && operator != to) assertEq(staker.balanceOf(operator), 0); // HERE
+        assertEq(staker.balanceOf(from), from == to ? mintAmount : mintAmount - transferAmount);
+        assertEq(staker.balanceOf(to), from == to ? mintAmount : transferAmount);
+    }
+
+    function testFuzz_transferFromWithoutApproval(
+        uint256 fromId,
+        uint256 operatorId,
+        uint256 toId,
+        uint80 transferAmount,
+        uint80 mintAmount
+    ) public {
+        address operator = _idToAddress(operatorId);
+        address from = _idToAddress(fromId);
+        address to = _idToAddress(toId);
+
+        vm.assume(operator != from);
+
+        transferAmount = uint80(_bound(transferAmount, 1, type(uint80).max));
+        mintAmount = uint80(_bound(mintAmount, transferAmount, type(uint80).max));
+
+        _mint(from, mintAmount);
+
+        vm.expectRevert();
+        vm.prank(operator);
+        staker.transferFrom(from, to, transferAmount);
+    }
+
+    function testFuzz_transferFromExceedAllowance(
+        uint80 transferAmount,
+        uint80 mintAmount,
+        uint256 allowedAmount
+    ) public {
+        transferAmount = uint80(_bound(transferAmount, 1, type(uint80).max));
+        mintAmount = uint80(_bound(mintAmount, transferAmount, type(uint80).max));
+        allowedAmount = _bound(allowedAmount, 0, transferAmount - 1);
+
+        _mint(bob, mintAmount);
+
+        vm.prank(bob);
+        staker.approve(alice, allowedAmount);
+
+        vm.expectRevert();
+        vm.prank(alice);
+        staker.transferFrom(bob, alice, transferAmount);
+    }
+}
