fb26d2faae539f36cde63c6f8e810e5beb182d6f
Codonyat <nil.garcia@gmail.com>
2025-01-20 14:34:54 +0100

    added uniswap quoter addresses

--- src/libraries/AddressesSepolia.sol
+++ src/libraries/AddressesSepolia.sol
@@ -5,6 +5,7 @@ library AddressesSepolia {
     address internal constant ADDR_UNISWAPV3_FACTORY = 0x0227628f3F023bb0B980b67D528571c95c6DaC1c;
     address internal constant ADDR_UNISWAPV3_OWNER = 0x1a9C8182C09F50C8318d769245beA52c32BE35BC;
     address internal constant ADDR_UNISWAPV3_POSITION_MANAGER = 0x1238536071E1c677A632429e3655c799b22cDA52;
+    address internal constant ADDR_UNISWAP_QUOTER_V2 = 0xEd1f6473345F45b75F8179591dd5bA1888cf2FB3;
     address internal constant ADDR_USDT = 0xC959483DBa39aa9E78757139af0e9a2EDEb3f42D;
     address internal constant ADDR_WETH = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
     address internal constant ADDR_USDC = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238;
