2aa9d3c435eaf86c23fbecf9b48f8d601d38e067
Codonyat <nil.garcia@gmail.com>
2025-06-02 18:03:19 +0200

    bugfix: the portion of fees derived to stakers was 5% instead of 20%

--- src/libraries/Fees.sol
+++ src/libraries/Fees.sol
@@ -8,12 +8,13 @@ import {SirStructs} from "./SirStructs.sol";
  */
 
 library Fees {
-    /** @notice APES pay a fee to the LPers when they mint/burn APE
-        @notice If a non-zero tax is set for the vault, 10% of the fee is sent to SIR stakers
-        @param collateralDepositedOrOut Amount of collateral deposited or taken out by the apes
-        @param baseFee Base fee in basis points per unit of liquidity
-        @param leverageTier Tier of the vault
-        @param tax Tax in basis points charged to the apes for getting SIR
+    /**
+     *  @notice APES pay a fee to the LPers when they mint/burn APE
+     *  @notice If a non-zero tax is set for the vault, up to a maximum of 20% of the fee is sent to SIR stakers.
+     *  @param collateralDepositedOrOut Amount of collateral deposited or taken out by the apes
+     *  @param baseFee Base fee in basis points per unit of liquidity
+     *  @param leverageTier Tier of the vault
+     *  @param tax Tax in basis points charged to the apes for getting SIR
      */
     function feeAPE(
         uint144 collateralDepositedOrOut,
@@ -38,17 +39,18 @@ library Fees {
             uint256 totalFees = collateralDepositedOrOut - fees.collateralInOrWithdrawn;
 
             // Depending on the tax, between 0 and 20% of the fee is for SIR stakers
-            fees.collateralFeeToStakers = uint144((totalFees * tax) / (20 * uint256(type(uint8).max))); // Cannot overflow cuz fee is uint144 and tax is uint8
+            fees.collateralFeeToStakers = uint144((totalFees * tax) / (5 * uint256(type(uint8).max))); // Cannot overflow cuz fee is uint144 and tax is uint8
 
             // The rest is sent to the gentlemen, if there are none, then it is POL
             fees.collateralFeeToLPers = uint144(totalFees) - fees.collateralFeeToStakers;
         }
     }
 
-    /** @notice LPers pay a fee to the protocol when they mint TEA
-        @notice collateralFeeToLPers is the fee paid to the protocol (not all LPers)
-        @param collateralDeposited Amount of collateral deposited by the LPers
-        @param lpFee Fee in basis points charged to LPers and sent to the protocol
+    /**
+     *  @notice LPers pay a fee to the protocol when they mint TEA
+     *  @notice collateralFeeToLPers is the fee paid to the protocol (not all LPers)
+     *  @param collateralDeposited Amount of collateral deposited by the LPers
+     *  @param lpFee Fee in basis points charged to LPers and sent to the protocol
      */
     function feeMintTEA(uint144 collateralDeposited, uint16 lpFee) internal pure returns (SirStructs.Fees memory fees) {
         unchecked {
--- test/Fees.t.sol
+++ test/Fees.t.sol
@@ -52,10 +52,24 @@ contract FeesTest is Test {
         assertGe(totalFeeUpperBound, totalFee, "Total fee too high");
         assertEq(
             fees.collateralFeeToStakers,
-            (uint256(totalFee) * tax) / (uint256(20) * type(uint8).max),
+            (uint256(totalFee) * tax) / (uint256(5) * type(uint8).max),
             "Stakers fee incorrect"
         );
         assertEq(fees.collateralFeeToLPers, totalFee - fees.collateralFeeToStakers, "LPers fee incorrect");
+    }
+
+    function testFuzz_maxFeeToStakers(uint144 collateralDepositedOrOut, uint16 baseFee, int8 leverageTier) public pure {
+        uint8 tax = type(uint8).max;
+
+        // Constraint leverageTier to supported values
+        leverageTier = int8(_bound(leverageTier, SystemConstants.MIN_LEVERAGE_TIER, SystemConstants.MAX_LEVERAGE_TIER));
+
+        SirStructs.Fees memory fees = Fees.feeAPE(collateralDepositedOrOut, baseFee, leverageTier, tax);
+
+        uint256 totalFee = uint256(fees.collateralFeeToStakers) + fees.collateralFeeToLPers;
+        assertEq(fees.collateralInOrWithdrawn + totalFee, collateralDepositedOrOut, "wrong collateral + fee");
+
+        assertEq(fees.collateralFeeToStakers, (totalFee * 20) / 100, "Max fee to stakers is not 20%");
     }
 
     function testFuzz_feeMintTEA(uint144 collateralDeposited, uint16 lpFee) public pure {
